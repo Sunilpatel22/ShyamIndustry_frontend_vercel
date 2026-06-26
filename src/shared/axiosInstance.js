863d9036e229546ab05a6b28fb9d06c7220d659c
Sunil patel <sunilkumarpatel7985@gmail.com>
2026-06-27 03:57:40 +0530

    axios

--- src/shared/axiosInstance.js
+++ src/shared/axiosInstance.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+// Ensure there is a full 'https://' protocol present in the string fallback
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://shyamindustry-backend.vercel.app';
+
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
   timeout: 40000, 
